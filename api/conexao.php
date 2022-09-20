7dd3f46f2254e208c0c63f37de059bc483af83ac
Erick Jonathan <zxjonathanxz22@gmail.com>
2022-09-19 23:22:40 -0300

    agr si

--- api/conexao.php
+++ api/conexao.php
@@ -2,6 +2,6 @@
 define('HOST', '127.0.0.1');
 define('USUARIO', 'root');
 define('SENHA', 'toperick123*');
-define('DB', 'ListaConvidados');
+define('DB', 'Convidados');
 
 $conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die('Não foi possível conectar');
