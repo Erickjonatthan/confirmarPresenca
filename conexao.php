17727fdf175d1f86d405e498a9d7c89f156cc259
Erick Jonathan <zxjonathanxz22@gmail.com>
2022-09-20 00:34:26 -0300

    atualizando conexao

--- conexao.php
+++ conexao.php
@@ -1,7 +1,7 @@
 <?php
-define('HOST', '127.0.0.1');
-define('USUARIO', 'root');
-define('SENHA', 'toperick123*');
-define('DB', 'convidados');
+define('HOST', 'us-cdbr-east-06.cleardb.net');
+define('USUARIO', 'b11f2ad5c86b46');
+define('SENHA', '47553f0a');
+define('DB', 'heroku_4ae7b9e0f22d48f');
 
 $conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die('Não foi possível conectar');
