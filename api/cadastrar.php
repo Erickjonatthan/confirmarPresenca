280e68a9a927f9f3c8bbed4dd5b41b52a517f952
Erick Jonathan <zxjonathanxz22@gmail.com>
2022-09-19 23:18:44 -0300

    att

--- api/cadastrar.php
+++ api/cadastrar.php
@@ -6,23 +6,14 @@ $nome = mysqli_real_escape_string($conexao, trim($_POST['nome']));
 $resposta = mysqli_real_escape_string($conexao, trim($_POST['resposta']));
 
 
-$sql = "select count(*) as total from ListaConvidados where nome = '$nome'";
-$result = mysqli_query($conexao, $sql);
-$row = mysqli_fetch_assoc($result);
-
-if($row['total'] == 1) {
-	$_SESSION['usuario_existe'] = true;
-	header('Location: index.php');
-	exit;
-}
 
 $sql = "INSERT INTO ListaConvidados (nome, resposta, data_cadastro) VALUES ('$nome', '$resposta', NOW())";
 
 if($conexao->query($sql) === TRUE) {
-	$_SESSION['status_cadastro'] = true;
+    $conexao->close();
 }
 
-$conexao->close();
+
 
 header('Location: index.php');
 exit;
