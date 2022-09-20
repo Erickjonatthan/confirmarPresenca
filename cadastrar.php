3f978f9d4b13e45360af290b40ac22d0a7d05a7e
Erick Jonathan <zxjonathanxz22@gmail.com>
2022-09-20 00:43:02 -0300

    aa

--- cadastrar.php
+++ cadastrar.php
@@ -1,19 +1,28 @@
 <?php
-
+session_start();
 include("conexao.php");
 
 $nome = mysqli_real_escape_string($conexao, trim($_POST['nome']));
-$resposta = mysqli_real_escape_string($conexao, trim($_POST['resposta']));
+$resposta = mysqli_real_escape_string($conexao, trim($_POST['usuario']));
 
 
+$sql = "select count(*) as total from listaconvidados where nome = '$nome'";
+$result = mysqli_query($conexao, $sql);
+$row = mysqli_fetch_assoc($result);
 
-$sql = "INSERT INTO ListaConvidados (nome, resposta, data_cadastro) VALUES ('$nome', '$resposta', NOW())";
-
-if($conexao->query($sql) === TRUE) {
-    $conexao->close();
+if($row['total'] == 1) {
+	$_SESSION['usuario_existe'] = true;
+	header('Location: cadastro.php');
+	exit;
 }
 
+$sql = "INSERT INTO listaconvidados (nome, resposta, data_cadastro) VALUES ('$nome', '$resposta', NOW())";
 
+if($conexao->query($sql) === TRUE) {
+	$_SESSION['status_cadastro'] = true;
+}
 
-header('Location: confirma.php');
+$conexao->close();
+
+header('Location: cadastro.php');
 exit;
