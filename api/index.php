d58886f2faf66afb8fdc8df736caf22ad4cc3711
Erick Jonathan <zxjonathanxz22@gmail.com>
2022-09-19 23:20:38 -0300

    ok

---
+++ api/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="responsivo.css" />
+    <title>Confirme sua presença </title>
+
+</head>
+
+<body>
+    <div class="caixa">
+        <h4><ins>Obrigado!</ins></h4>
+
+        
+
+    </div>
+
+</body>
+
+</html>
